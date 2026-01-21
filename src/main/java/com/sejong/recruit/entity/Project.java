2b4dd164822720937de3263b4d6fe9748a7b5db0
chaegong <138372298+chaechae23011667@users.noreply.github.com>
2026-01-21 17:00:11 +0900

    feat: 프로젝트 멤버(ProjectMember) 테이블 추가 및 연관관계 매핑 완료

--- src/main/java/com/sejong/recruit/entity/Project.java
+++ src/main/java/com/sejong/recruit/entity/Project.java
@@ -1,11 +1,14 @@
 package com.sejong.recruit.entity;
 
 import com.sejong.recruit.global.common.BaseTimeEntity;
+import com.sejong.recruit.entity.User;
 import com.sejong.recruit.enums.ProjectStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 프로젝트 모집 공고 엔티티
@@ -39,7 +42,17 @@ public class Project extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private ProjectStatus status;
 
+    // [추가] 프로젝트 멤버와의 양방향 연관관계
+    @Builder.Default
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProjectMember> members = new ArrayList<>();
+
     public void changeStatus(ProjectStatus status) {
         this.status = status;
+    }
+
+    // 멤버 추가 편의 메서드
+    public void addMember(ProjectMember member) {
+        this.members.add(member);
     }
 }
